ddd55a926caa8d03932684964e030dc807c0bd47
map1211 <twins887605@hansung.ac.kr>
2020-12-29 15:22:06 +0900

    2020.12.29 lsb 최초 커밋

---
+++ lsb/src/main/java/com/thinkit/test/Controller.java
@@ -0,0 +1,7 @@
+package com.thinkit.test;
+
+public class Controller {
+
+
+	
+}
---
+++ lsb/src/test/java/com/thinkit/test/LsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkit.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
