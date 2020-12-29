25c525ca3be213d271f7ffa534af52bd39856468
map1211 <twins887605@hansung.ac.kr>
2020-12-29 18:02:15 +0900

    2020.12.29 lsb (master) last

--- lsb/src/main/java/com/thinkit/test/controller/Controller.java
+++ lsb/src/main/java/com/thinkit/test/controller/Controller.java
@@ -3,8 +3,6 @@ package com.thinkit.test.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sun.tools.classfile.Opcode.Set;
-
 @RestController
 public class Controller {
 	
