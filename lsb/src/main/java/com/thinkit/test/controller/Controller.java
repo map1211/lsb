010417ee40b2faeb8fba28a67b538b92fa6633b7
map1211 <twins887605@hansung.ac.kr>
2020-12-29 15:44:38 +0900

    2020.12.29 lsb (develop) - RestController index 추가

---
+++ lsb/src/main/java/com/thinkit/test/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.thinkit.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
