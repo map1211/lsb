69dfd61854262452908c4d9e536d28ad8206ed8d
map1211 <twins887605@hansung.ac.kr>
2020-12-29 15:27:50 +0900

    2020.12.29 lsb branch(develop) - 1st commit and push

--- lsb/src/main/java/com/thinkit/test/LsbApplication.java
+++ lsb/src/main/java/com/thinkit/test/LsbApplication.java
@@ -8,7 +8,7 @@ public class LsbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LsbApplication.class, args);
-		System.out.println("Branch Master!!!");
+		System.out.println("Branch Develop!!!");
 	}
 	
 
